53b5cfd26b8c62a1b4f3cafb3727ac589be06d52
imilic <dzimi82@gmail.com>
2025-08-20 14:48:40 +0200

    f

---
+++ HolestPay/HolestPay/Observer/AddChargeButton.php
@@ -0,0 +1,49 @@
+<?php
+namespace HolestPay\HolestPay\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use HolestPay\HolestPay\Controller\Adminhtml\Order\Charge as ChargeController;
+
+class AddChargeButton implements ObserverInterface
+{
+    /** @var OrderRepositoryInterface */
+    private $orderRepository;
+
+    public function __construct(OrderRepositoryInterface $orderRepository)
+    {
+        $this->orderRepository = $orderRepository;
+    }
+
+    public function execute(Observer $observer)
+    {
+        $container = $observer->getEvent()->getData('container');
+        if (!$container || $container->getId() !== 'sales_order_view') {
+            return;
+        }
+        $request = $container->getRequest();
+        $orderId = (int)$request->getParam('order_id');
+        if (!$orderId) {
+            return;
+        }
+        try {
+            $order = $this->orderRepository->get($orderId);
+        } catch (\Throwable $e) {
+            return;
+        }
+        if (!ChargeController::isAvailable($order)) {
+            return;
+        }
+        $container->addButton(
+            'holestpay_order_charge',
+            [
+                'label' => __('Charge'),
+                'class' => 'primary',
+                'onclick' => sprintf("setLocation('%s')", $container->getUrl('holestpayadmin/order/charge', ['order_id' => $orderId]))
+            ]
+        );
+    }
+}
+
+
